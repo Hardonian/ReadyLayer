b531906cf5434518f3467391c962926970979277
Scott Hardie <scottrmhardie@gmail.com>
2026-01-30 19:19:31 -0500

    contd python

---
+++ services/worker-py/src/handlers/__init__.py
@@ -0,0 +1,17 @@
+"""Job type handlers registry."""
+
+from src.handlers.base import register_handler, get_handler, list_registered_handlers, BaseHandler, JobResult
+
+# Import and register all handlers
+from src.handlers.ingest_normalize import IngestNormalizeHandler
+from src.handlers.recon_run import ReconRunHandler
+from src.handlers.anomaly_score import AnomalyScoreHandler
+from src.handlers.eval_run import EvalRunHandler
+
+__all__ = [
+    "register_handler",
+    "get_handler", 
+    "list_registered_handlers",
+    "BaseHandler",
+    "JobResult",
+]
